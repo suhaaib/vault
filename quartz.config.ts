531fa046d9bd459cbb6c92ef8562682ac3b696c9
suhaaib <161098485+suhaaib@users.noreply.github.com>
2024-02-23 22:29:00 -0500

    commit

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Suhaib's Working Notes",
+    pageTitle: "Working Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
